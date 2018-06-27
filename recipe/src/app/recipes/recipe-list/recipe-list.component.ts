716708914d37f9e14d01da0135636eaa9311eea7
darkfinger <892810dkcbak@G>
2018-06-27 18:36:35 -0400

    recipes

--- recipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,8 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
- recipes: Recipe[] = [new Recipe('guacamole', 'a guacamole recipe for 4 mouth', 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjF993J4PTbAhWhtlkKHYGrC1YQjRx6BAgBEAU&url=https%3A%2F%2Fwww.simplyrecipes.com%2Frecipes%2Fperfect_guacamole%2F&psig=AOvVaw0uSSDipf10fAf0jqTjUokt&ust=1530220186687710')];
+  recipes: Recipe[] = [new Recipe('guacamole', 'a guacamole recipe for 4 mouth',
+      'https://www.inspiredtaste.net/wp-content/uploads/2009/10/Easy-Homemade-Guacamole-Recipe-1-1200.jpg')];
   constructor() { }
 
   ngOnInit() {
--- recipe/src/app/shopping-list/shoping-list.component.ts
+++ recipe/src/app/shopping-list/shoping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shoping-list',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shoping-list.component.css']
 })
 export class ShopingListComponent implements OnInit {
-
+ ingredients: Ingredient[] = [new Ingredient('Avocado', 6), new Ingredient('tomato', 2)];
   constructor() { }
 
   ngOnInit() {
