c53bcbcd60d06d993da5a57ecb1eb679ee323f47
darkfinger <892810dkcbak@G>
2018-06-27 17:33:20 -0400

    recipes

--- recipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+ recipes: Recipe[] = [new Recipe('guacamole', 'a guacamole recipe for 4 mouth', 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjF993J4PTbAhWhtlkKHYGrC1YQjRx6BAgBEAU&url=https%3A%2F%2Fwww.simplyrecipes.com%2Frecipes%2Fperfect_guacamole%2F&psig=AOvVaw0uSSDipf10fAf0jqTjUokt&ust=1530220186687710')];
   constructor() { }
 
   ngOnInit() {
