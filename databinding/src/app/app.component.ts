af12a70dac91564061026d4fbb2f029328dc9bcb
darkfinger <892810dkcbak@G>
2018-06-28 16:17:46 -0400

    data biending

--- databinding/src/app/app.component.ts
+++ databinding/src/app/app.component.ts
@@ -6,8 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{ type: 'server', name: 'testServer', content: 'just a test'}];
 
+    onServerAdded(event: {name: string, content: string}) {
+        this.serverElements.push({
+            type: 'server',
+            name: event.name,
+            content: event.content
+        });
+    }
 
-
+    onBlueprintAdded(blueprintData: {name: string, content: string}) {
+        this.serverElements.push({
+            type: 'blueprint',
+            name: blueprintData.name,
+            content: blueprintData.content
+        });
+    }
 }
